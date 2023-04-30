cc25616012858ab28e051e38acf831a52d435181
DexterRusike <85769224+DexterRusike@users.noreply.github.com>
2023-04-30 23:39:32 +0100

    Update models.py

--- FamWeb3/chore_site/chore/models.py
+++ FamWeb3/chore_site/chore/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
 
 # Create your models here.
